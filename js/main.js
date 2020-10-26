0e1bab5d2c6b6830d949ee2976b260122ecb1549
Niko Kareno <niko.kareno@tuni.fi>
2020-10-26 10:02:10 +0200

    Updates

--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@ const menu = document.querySelector(".menu");
 const menuNav = document.querySelector(".menu-nav");
 const menuBranding = document.querySelector(".menu-branding");
 const navItems = document.querySelectorAll(".nav-item");
+const sizeBox = document.querySelector(".size");
 
 // Set Initial State of Menu
 let showMenu = false;
@@ -30,4 +31,32 @@ function toggleMenu() {
         //Set Menu State
         showMenu = false;
     }
+}
+
+// Resize project page box
+
+sizeBox.addEventListener("mouseup", setSize);
+
+function setSize() {
+    if (parseInt(sizeBox.style.width) > 1171) {
+        console.log("It's huge!");
+        sizeBox.style["grid-template-columns"] = "repeat(4, 1fr)";
+    } else if (
+        769 < parseInt(sizeBox.style.width) &&
+        parseInt(sizeBox.style.width) < 1171
+    ) {
+        console.log("It's large!");
+        sizeBox.style["grid-template-columns"] = "repeat(3, 1fr)";
+    } else if (
+        500 < parseInt(sizeBox.style.width) &&
+        parseInt(sizeBox.style.width) < 769
+    ) {
+        console.log("It's medium!");
+        sizeBox.style["grid-template-columns"] = "repeat(2, 1fr)";
+    } else {
+        console.log("It's small!");
+        sizeBox.style["grid-template-columns"] = "repeat(1fr)";
+    }
+
+    console.log(sizeBox.style.width);
 }
